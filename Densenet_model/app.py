b200df8c7b72d39269085f04ec134118cf9ff6f5
Paras Munoli <parasmunoli@gmail.com>
2024-09-26 10:49:16 +0530

    Update app.py

--- Densenet_model/app.py
+++ Densenet_model/app.py
@@ -1,17 +1,19 @@
 from flask import Flask, request, jsonify
 from PIL import Image
 import numpy as np
-import joblib
 import tensorflow as tf
 from flask_cors import CORS
 import gdown
-import os  # Added import for os module
+import os
+
 
 app = Flask(__name__)
 CORS(app)
 
+
 model_id = '1OYIlqOvuQgD4OLKA0HAa5HCb_GovcWvI'
 model_name = 'densenet'
+
 
 class_labels = [
     "Tomato Bacterial spot",
@@ -26,16 +28,22 @@ class_labels = [
     "Tomato Yellow Leaf Curl Virus"
 ]
 
+
 # Use dynamic path based on current working directory
 model_dir = os.path.join(os.getcwd(), 'Densenet_model')
 if not os.path.exists(model_dir):
     os.makedirs(model_dir)
 
-model_path = os.path.join(model_dir, f'{model_name}_model.sav')  # Using .sav format
+
+model_path = os.path.join(model_dir, f'{model_name}_model.h5')
+
+
 gdown.download(f'https://drive.google.com/uc?id={model_id}', model_path, quiet=False)
 
-# Load the model using joblib
-model = joblib.load('Densenet_model/densenet_model.sav')
+
+# Load the model using TensorFlow
+model = tf.keras.models.load_model(model_path)
+
 
 def preprocess_image(image, target_size):
     image = image.resize(target_size)
@@ -43,14 +51,17 @@ def preprocess_image(image, target_size):
     image = np.expand_dims(image, axis=0)
     return image
 
+
 def get_model_prediction(model, image):
     prediction = model.predict(image)
     return np.argmax(prediction, axis=1)[0]
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
     if 'image' not in request.files:
         return jsonify({'error': 'No image file provided'}), 400
+
 
     image_file = request.files['image']
     try:
@@ -58,18 +69,23 @@ def predict():
     except:
         return jsonify({'error': 'Invalid image format'}), 400
 
+
     preprocessed_image = preprocess_image(image, target_size=(224, 224))
+
 
     try:
         prediction = class_labels[get_model_prediction(model, preprocessed_image)]
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
+
     return jsonify({'prediction': prediction})
+
 
 @app.route('/status', methods=['GET'])
 def status():
     return jsonify({"success": True, "code": 200})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
